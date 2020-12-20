dd802bddb8c0941f027e8d845673ac767d22d9c1
Damian <damiandanielvigo@gmail.com>
2020-12-19 23:49:35 -0300

    add-home

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+import darkMode from './components/darkMode.js'
+import stickyFooter from './components/sticky_footer.js'
+import hamburguerMenu from './components/hamburguer_menu.js'
+import scrollTopButton from './components/scroll-boton.js'
+import searchFilters from './components/search_filter.js';
+
+
+const d = document;
+const w = window;
+
+d.addEventListener('DOMContentLoaded', (e) => {
+  darkMode('#switch');
+  stickyFooter('.header');
+  scrollTopButton('.scroll-top-btn');
+  searchFilters('.card-filter-input', '.cards-filter__card')
+});
+
+hamburguerMenu('.panel-btn', '.panel', '.panel__menu-a');
+
+const overlay = document.getElementById('overlay');
+d.querySelectorAll('.cards-filter__img').forEach((el) => {
+  el.addEventListener('click', () => {
+    const rute = el.getAttribute('src');
+    const description = el.parentNode.dataset.description;
+    overlay.classList.add('active');
+    d.querySelector('.overlay__img').src = rute;
+    d.querySelector('.overlay__description').innerHTML = description;
+  });
+});
+
+d.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', (e) => {
+  // overlay.classList.remove('active');
+  e.target.id === 'overlay' ? overlay.classList.remove('active') : '';
+});
